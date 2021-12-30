204b3b15198bbe223ca5181fdf9369dcf0c5afe2
Fatkhur Rony <ronyelkahfi@gmail.com>
2021-12-30 17:04:14 +0700

    perm main : add error id not found on helper

--- helpers/error.go
+++ helpers/error.go
@@ -6,4 +6,5 @@ var (
 	ErrNotFound      = errors.New("data not found")
 	ErrDbServer      = errors.New("db server error")
 	ErrEmptyUrlParam = errors.New("Empty url param")
+	ErrIdNotFound    = errors.New("Id not found")
 )
