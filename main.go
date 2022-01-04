e3984daf86ea171ba3ce8950232c9f96915340bc
Fatkhur Rony <ronyelkahfi@gmail.com>
2022-01-04 20:18:20 +0700

    core : uncomment main.go

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
 import (
+	_categoryUsecase "final-project/business/categories"
+	_productUsecase "final-project/business/products"
+	_transactionUsecase "final-project/business/transactions"
+	_userUsecase "final-project/business/users"
+	_dbDriver "final-project/drivers/databases"
+	_categoryRepo "final-project/drivers/databases/categories"
+	_productRepo "final-project/drivers/databases/products"
+	_transactionDetailRepo "final-project/drivers/databases/transactionDetails"
+	_transactionRepo "final-project/drivers/databases/transactions"
+	_userRepo "final-project/drivers/databases/users"
 	"log"
+	"time"
+
+	_route "final-project/apps/routes"
+	_categoryController "final-project/controllers/categories"
+	_productController "final-project/controllers/products"
+	_transactionController "final-project/controllers/transactions"
+	_userController "final-project/controllers/users"
 
 	"github.com/labstack/echo/v4"
 	"github.com/spf13/viper"
@@ -21,44 +38,42 @@ func init() {
 }
 
 func main() {
-	// configDB := _dbDriver.ConfigDB{
-	// 	DB_Username: viper.GetString(`database.user`),
-	// 	DB_Password: viper.GetString(`database.pass`),
-	// 	DB_Host:     viper.GetString(`database.host`),
-	// 	DB_Port:     viper.GetString(`database.port`),
-	// 	DB_Database: viper.GetString(`database.name`),
-	// }
+	configDB := _dbDriver.ConfigDB{
+		DB_Username: viper.GetString(`database.user`),
+		DB_Password: viper.GetString(`database.pass`),
+		DB_Host:     viper.GetString(`database.host`),
+		DB_Port:     viper.GetString(`database.port`),
+		DB_Database: viper.GetString(`database.name`),
+	}
 
-	// db := configDB.ConnectDB()
-	// timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
+	db := configDB.ConnectDB()
+	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
 
-	// userRepo := _userRepo.NewUserRepository(db)
-	// userUseCase := _userUsecase.NewUserUsecase(userRepo, timeoutContext)
-	// userController := _userController.NewUserController(userUseCase)
+	userRepo := _userRepo.NewUserRepository(db)
+	userUseCase := _userUsecase.NewUserUsecase(userRepo, timeoutContext)
+	userController := _userController.NewUserController(userUseCase)
 
-	// productRepo := _productRepo.NewProductRepository(db)
-	// productUseCase := _productUsecase.NewProductUsecase(productRepo, timeoutContext)
-	// productController := _productController.NewProductController(productUseCase)
+	productRepo := _productRepo.NewProductRepository(db)
+	productUseCase := _productUsecase.NewProductUsecase(productRepo, timeoutContext)
+	productController := _productController.NewProductController(productUseCase)
 
-	// categoryRepo := _categoryRepo.NewCategoryRepository(db)
-	// categoryUseCase := _categoryUsecase.NewCategoryUsecase(categoryRepo, timeoutContext)
-	// categoryController := _categoryController.NewCategoryController(categoryUseCase)
+	categoryRepo := _categoryRepo.NewCategoryRepository(db)
+	categoryUseCase := _categoryUsecase.NewCategoryUsecase(categoryRepo, timeoutContext)
+	categoryController := _categoryController.NewCategoryController(categoryUseCase)
 
-	// transactionRepo := _transactionRepo.NewTransactionRepository(db)
-	// transactionDetailRepo := _transactionDetailRepo.NewTransDetailRepository(db)
-	// transactionUseCase := _transactionUsecase.NewTransactionUsecase(transactionRepo, transactionDetailRepo, timeoutContext)
-	// transactionController := _transactionController.NewTransactionController(transactionUseCase)
+	transactionRepo := _transactionRepo.NewTransactionRepository(db)
+	transactionDetailRepo := _transactionDetailRepo.NewTransDetailRepository(db)
+	transactionUseCase := _transactionUsecase.NewTransactionUsecase(transactionRepo, transactionDetailRepo, timeoutContext)
+	transactionController := _transactionController.NewTransactionController(transactionUseCase)
 
 	e := echo.New()
-	// routesInit := _route.ControllerList{
-	// 	UserController:        *userController,
-	// 	ProductController:     *productController,
-	// 	CategoryController:    *categoryController,
-	// 	TransactionController: *transactionController,
-	// }
-	// routesInit.RouteRegister(e)
-	e.GET("/", func(c echo.Context) error {
-		return c.String(200, "Hello World")
-	})
+	routesInit := _route.ControllerList{
+		UserController:        *userController,
+		ProductController:     *productController,
+		CategoryController:    *categoryController,
+		TransactionController: *transactionController,
+	}
+	routesInit.RouteRegister(e)
+
 	log.Fatal(e.Start(viper.GetString("server.address")))
 }
